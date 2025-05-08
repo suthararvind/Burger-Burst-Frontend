28368bb196de79102e57359f4425c80f6d64c5f5
Arvind Kumar <111381098+suthararvind@users.noreply.github.com>
2025-05-08 22:03:31 +0530

    Update Founder.jsx

--- src/components/home/Founder.jsx
+++ src/components/home/Founder.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import me from "../../assets/founder.webp";
+import me from "../../assets/profileburger.jpg";
 const Founder = () => {
   const options = {
     initial: {
